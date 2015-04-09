a29f045899c68b9c56c356a1eba8b391d57388a0
thedean11 <edean1010@gmail.com>
2015-04-09 10:11:31 -0500

    cleanup and commenting

--- cheers.rb
+++ cheers.rb
@@ -1,5 +1,6 @@
 require 'date'
 
+#Enter name
 def enter_name
 	name = ""
 	while name.empty?
@@ -9,6 +10,7 @@ def enter_name
 	name
 end
 
+#Enter Name Action
 name = enter_name
 name.gsub!(/[^\w]/,"")
 an_letters = "AEIOFHLMRSX"
@@ -18,13 +20,14 @@ name.each_char do |letter|
 end
 puts name + "'s just GRAND!"
 
+#Record Birthday Action
 puts "Hey #{name}, what's your birthday(M-D-Y)?"
 birthday = Date.strptime(gets.chomp, '%m-%d-%Y').yday
 now = DateTime.now.yday
 diff = birthday - now
 if diff < 0 && diff > -180
 	puts "Awesome!  Your birthday was #{diff*-1} days ago! Happy Belated Birthday!"
-	elsif diff < 0 && diff < -180
+	elsif diff < -180
 		puts "Awesome!  Your birthday is in #{365-(diff*-1)} days! Happy Birthday in advance!"
 	else puts "Awesome!  Your birthday is in #{diff} days! Happy Birthday in advance!"
 end
